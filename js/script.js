db6eed7cc30ae654d3d874c980993016ddcbb708
Dmitriy <dmitriy.kostenko94@gmail.com>
2021-12-12 11:02:47 +0700

    add js file

---
+++ js/script.js
@@ -0,0 +1,36 @@
+const left = document.querySelector('.button.left'),
+      right = document.querySelector('.button.right'),
+      sidebar = document.querySelector('.sidebar'),
+      textBlocks = sidebar.querySelector('.sidebar > div'),
+      main = document.querySelector('.main'),
+      slidesCount = main.querySelectorAll('.main > div').length;
+console.log(slidesCount);
+
+let slideIndex = 0;
+sidebar.style.left = `-${(slidesCount - 1) * 100}vw`;
+
+left.addEventListener('click', () => {
+    changeSlide('left');
+});
+right.addEventListener('click', () => {
+    changeSlide('right');
+});
+
+function changeSlide(btn){
+    if(btn === 'right'){
+        slideIndex++;
+        if(slideIndex === slidesCount){
+            slideIndex = 0;
+        }
+    } else if (btn === 'left') {
+        slideIndex--;
+        if(slideIndex < 0){
+            slideIndex = slidesCount - 1;
+        }
+    }
+    console.log(`SlideIndex = ${slideIndex}`);
+
+    
+    sidebar.style.transform =  `translateX(${100 * slideIndex}vw)`;
+    main.style.transform = `translateX(-${100 * slideIndex}vw)`;
+}
